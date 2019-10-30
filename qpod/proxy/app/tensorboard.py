cabf62e2e571d53b18d50895799aa525ccbf2c5e
Bibo Hao <haobibo@users.noreply.github.com>
2019-10-30 19:46:35 +0800

    Update tensorboard.py

--- qpod/proxy/app/tensorboard.py
+++ qpod/proxy/app/tensorboard.py
@@ -11,8 +11,9 @@ def setup_tensorboard():
         return [
             executable,
             '--port=' + str(port),
+            '--bind_all',
             '--logdir=/tmp/tensorboard',
-            #'--path_prefix=/tensorboard/',
+            # '--path_prefix=/tensorboard/',
             '--purge_orphaned_data=true',
             '--window_title=QPod - Tensorboard'
         ]
@@ -21,5 +22,6 @@ def setup_tensorboard():
         'name': 'tensorboard',
         'command': _get_tensorboard_cmd,
         'environment': {},
-        'absolute_url': True
+        'absolute_url': True,
+        'timeout': 20
     }
